e2e5fcef8377af12bab2e1c7343b139a869d447d
hirokoymj <hiroko@hirokoymj.com>
2025-07-17 22:38:38 -0500

    updated vite config file

--- src/react-hooks-useMemo/Demo1.tsx
+++ src/react-hooks-useMemo/Demo1.tsx
@@ -31,7 +31,7 @@ const Demo1 = () => {
   return (
     <div style={{ border: '10px solid black' }}>
       <h1>{apiKey}</h1>
-      <p style={{ color: `${color}`, fontSize: '2rem' }}>This is App component on 07/17</p>
+      <p style={{ color: `${color}`, fontSize: '2rem' }}>This is App component on 07/17 at 10:36PM.</p>
       <button onClick={() => setColor('red')}>Change color</button>
       <br />
       Counter: {counter}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import EnvironmentPlugin from 'vite-plugin-environment';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import EnvironmentPlugin from 'vite-plugin-environment';
 
-export default defineConfig({
-  plugins: [react(), EnvironmentPlugin(['API_KEY'])],
-  base: '/',
+// export default defineConfig({
+//   plugins: [react(), EnvironmentPlugin(['API_KEY'])],
+//   base: '/',
+// });
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: '/',
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  };
 });
